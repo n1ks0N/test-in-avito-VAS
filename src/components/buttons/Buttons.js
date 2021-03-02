5d80a6cddb92854d594a2f623b19eeee76306923
niksON <gromyakov05@mail.ru>
2021-03-03 00:21:06 +0300

    image post

--- src/components/buttons/Buttons.js
+++ src/components/buttons/Buttons.js
@@ -5,39 +5,27 @@ import gifshot from 'gifshot';
 import './Buttons.css';
 
 const Buttons = ({ notice, banner: { width, height, properties, time } }) => {
-	const html = `<div class="banner" id="el0"> <p class="banner__text">${
-		properties[0].text
-	}</p></div><div class="banner" id="el1"> <p class="banner__text">${
-		properties[1].text
-	}</p></div><div class="banner" id="el2"> <p class="banner__text">${
-		properties[2].text
-	}</p></div> <style> .banner { position: relative; text-align: center; cursor: pointer; display: flex; flex-direction: row; align-items: center; justify-content: center; width: ${width}px; height: ${height}px; background-color: #fff; background-repeat: no-repeat; border: 1px solid #000; }#el0 {color: ${
-		properties[0].color
-	};font-weight: ${properties[0].bold ? 'bold' : 'normal'};font-style: ${
-		properties[0].italic ? 'italic' : 'normal'
-	};font-size: ${properties[0].size}px;font-family: ${properties[0].font[0]};
+	const html = `<div class="banner" id="el0"> <p class="banner__text">${properties[0].text
+		}</p></div><div class="banner" id="el1"> <p class="banner__text">${properties[1].text
+		}</p></div><div class="banner" id="el2"> <p class="banner__text">${properties[2].text
+		}</p></div> <style> .banner { position: relative; text-align: center; cursor: pointer; display: flex; flex-direction: row; align-items: center; justify-content: center; width: ${width}px; height: ${height}px; background-color: #fff; background-repeat: no-repeat; border: 1px solid #000; }#el0 {color: ${properties[0].color
+		};font-weight: ${properties[0].bold ? 'bold' : 'normal'};font-style: ${properties[0].italic ? 'italic' : 'normal'
+		};font-size: ${properties[0].size}px;font-family: ${properties[0].font[0]};
 	background-image: url(${properties[0].image});
 	background-position: ${properties[0].left}px ${properties[0].top}px;
 	background-size: ${properties[0].imgSize}%;
-}#el1 {color: ${properties[1].color};font-weight: ${
-		properties[1].bold ? 'bold' : 'normal'
-	};font-style: ${properties[1].italic ? 'italic' : 'normal'};font-size: ${
-		properties[1].size
-	}px;font-family: ${
-		properties[1].font[0]
-	};display: none; background-image: url(${properties[1].image});
+}#el1 {color: ${properties[1].color};font-weight: ${properties[1].bold ? 'bold' : 'normal'
+		};font-style: ${properties[1].italic ? 'italic' : 'normal'};font-size: ${properties[1].size
+		}px;font-family: ${properties[1].font[0]
+		};display: none; background-image: url(${properties[1].image});
 	background-position: ${properties[1].left}px ${properties[1].top}px;
-	background-size: ${properties[1].imgSize}%;}#el2 {color: ${
-		properties[2].color
-	};font-weight: ${properties[2].bold ? 'bold' : 'normal'};font-style: ${
-		properties[2].italic ? 'italic' : 'normal'
-	};font-size: ${properties[2].size}px;font-family: ${
-		properties[2].font[2]
-	};display: none; background-image: url(${properties[2].image});
+	background-size: ${properties[1].imgSize}%;}#el2 {color: ${properties[2].color
+		};font-weight: ${properties[2].bold ? 'bold' : 'normal'};font-style: ${properties[2].italic ? 'italic' : 'normal'
+		};font-size: ${properties[2].size}px;font-family: ${properties[2].font[2]
+		};display: none; background-image: url(${properties[2].image});
 	background-position: ${properties[2].left}px ${properties[2].top}px;
-	background-size: ${
-		properties[2].imgSize
-	}%;} .banner__text { width: 100%; margin: 0; z-index: 1; overflow: hidden; line-height: 1.2em; max-height: 3.6em; text-align: center; } </style> <script>let count = 0; setInterval(() => {count < 2 ? count++ : count = 0;document.getElementById(\`el\${count ? count-1 : 2}\`).style.display = 'none';document.getElementById(\`el\${count}\`).style.display = 'flex';}, ${time}000)</script>`;
+	background-size: ${properties[2].imgSize
+		}%;} .banner__text { width: 100%; margin: 0; z-index: 1; overflow: hidden; line-height: 1.2em; max-height: 3.6em; text-align: center; } </style> <script>let count = 0; setInterval(() => {count < 2 ? count++ : count = 0;document.getElementById(\`el\${count ? count-1 : 2}\`).style.display = 'none';document.getElementById(\`el\${count}\`).style.display = 'flex';}, ${time}000)</script>`;
 	const [link, setLink] = useState(html);
 	const exportHTML = () => {
 		navigator.clipboard.writeText(html);
@@ -68,25 +56,22 @@ const Buttons = ({ notice, banner: { width, height, properties, time } }) => {
 									if (!obj.error) {
 										const image = obj.image;
 										setSrc(() => image);
-										let data = {
-											image: src
+										var myHeaders = new Headers();
+										myHeaders.append("Authorization", "Client-ID b606abe6f82246c");
+
+										var formdata = new FormData();
+										formdata.append("image", `${image}`)
+										var requestOptions = {
+											method: 'POST',
+											headers: myHeaders,
+											body: formdata,
+											redirect: 'follow'
 										};
-										fetch(
-											'https://api.imgbb.com/1/upload?expiration=600&key=9bfbbf5c0172756995a9ebc1990fa389',
-											{
-												method: 'POST',
-												headers: {
-													'Access-Control-Allow-Origin': '*',
-													'Content-Type': 'application/json;charset=utf-8'
-												},
-												body: JSON.stringify(data)
-											}
-										)
-											.then((response) => response.json())
-											.then((res) => {
-												console.log(res.data.url);
-												setLink(() => res.data.url);
-											});
+
+										fetch("https://api.imgur.com/3/image", requestOptions)
+											.then(response => response.text())
+											.then(result => console.log(result))
+											.catch(error => console.log('error', error));
 									}
 								}
 							);
